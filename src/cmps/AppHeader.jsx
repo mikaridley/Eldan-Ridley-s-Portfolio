6df0a32598a8726673df9b6b4df4e1bb034172a5
Mika Ridley <mikaridley26@gmail.com>
2026-02-11 12:54:42 +0200

    When on a page, make it hover style

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,17 +1,17 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/imgs/logo.png'
 
 export function AppHeader() {
   return (
     <header className="app-header">
       <div className="logo">
-        <img src={logo} />
+        <img src={logo} alt="Logo" />
       </div>
       <nav className="navigation">
-        <Link to="/home">Home</Link>
-        <Link to="/about-me">About Me</Link>
-        <Link to="/resume">Resume</Link>
-        <Link to="/linkedin">LinkedIn</Link>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/about-me">About Me</NavLink>
+        <NavLink to="/resume">Resume</NavLink>
+        <NavLink to="/linkedin">LinkedIn</NavLink>
       </nav>
     </header>
   )
