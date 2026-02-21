f3eb0a32de8ca2e22d629a9529cf960910863e47
Mika Ridley <mikaridley26@gmail.com>
2026-02-21 20:34:54 +0200

    switch logo

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,11 +1,16 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import logo from '../assets/imgs/logo.png'
+import logoBlack from '../assets/imgs/logo-black.png'
 
 export function AppHeader() {
+  const { pathname } = useLocation()
+  const isHome = pathname === '/' || pathname === '/home'
+  const logoSrc = isHome ? logo : logoBlack
+
   return (
     <header className="app-header">
       <div className="logo">
-        <img src={logo} alt="Logo" />
+        <img src={logoSrc} alt="Logo" />
       </div>
       <nav className="navigation">
         <NavLink to="/home">Home</NavLink>
